ec20c535a03996738141b6b5823a94719b64a3d8
lerinlouis <lerinkuriakose@gmail.com>
2023-06-29 14:47:17 +0530

    JS_Attempt

---
+++ GitStart/dom.js
@@ -0,0 +1,9 @@
+var headerTitle = document.getElementById('header-title');
+headerTitle.style.borderBottom = 'solid 3px #000';
+
+
+var titleAdd = document.getElementsByClassName('title');
+for (var i = 0; i < titleAdd.length; i++) {
+  titleAdd[i].style.color = 'green';
+  
+}
